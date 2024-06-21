1154ac57d11cb665b31ba847159f0c4a7ea5dc89
Sim4n6 <sim4n6@gmail.com>
2024-06-21 13:32:27 +0100

    Refactor analyze.sh script to improve readability and remove unnecessary code

--- analyze.sh
+++ analyze.sh
@@ -10,10 +10,10 @@ url=$1
 # append the number of the day in the year 
 directory="$2-$(date +%j)"
 
-# create directory if it doesn't exist
-if [ ! -d "./targets/$directory/js_files" ]; then
-  mkdir -p "./targets/$directory/js_files"
-fi
+# create directory if it doesn't exist, delete any way
+rm -rf "./targets/$directory/js_files"
+mkdir -p "./targets/$directory/js_files"
+
 
 # download the target JS file
 # echo "$url" | getJS --resolve --verbose --complete --insecure | tee "./targets/$directory/js.files" | sort -u | wget -N -P "./targets/$directory/js_files/" -i -
@@ -25,13 +25,20 @@ for jsfile in $(ls ./targets/$directory/js_files); do
 done
 
 # create CodeQL database
-if [ ! -d "./targets/$directory/codeql-database" ]; then
-  rm -rf "./targets/$directory/codeql-database"
-fi
-codeql database create "./targets/$directory/codeql-database" --threads=0 --overwrite --language=javascript --source-root "./targets/$directory/js_files/"
+codeql database create "./targets/$directory/codeql-database" --threads=0 --language=javascript --source-root "./targets/$directory/js_files/"
 
 # analyze the target JS file
 CODEQL_SUITES_PATH=$HOME/Codeql-Home/codeql-repo/javascript/ql/src/codeql-suites
 # --sarif-add-snippets 
-codeql database analyze --threads=0 --ram=16384 --format=sarif-latest --output="./targets/$directory/results-$directory.sarif" --  "./targets/$directory/codeql-database" "$CODEQL_SUITES_PATH/javascript-security-extended.qls"
+codeql database analyze --threads=0 --ram=16384 --format=sarif-latest --output="./targets/$directory/js_files/results-$directory.sarif" --  "./targets/$directory/codeql-database" "$CODEQL_SUITES_PATH/javascript-security-extended.qls"
 #codeql database analyze --threads=8 --format=sarif-latest --output="./targets/$directory/codeql-results.sarif" --  "./targets/$directory/codeql-database" "$CODEQL_SUITES_PATH/javascript-security-experimental.qls"
+
+
+# print outcome in "./targets/$directory/js_files/results-$directory.sarif"
+
+echo " =============== SUMMARY ==============="
+sarif summary "./targets/$directory/js_files/results-$directory.sarif"
+echo " =============== ------- ==============="
+
+echo "Analysis completed."
+echo "Results are in ./targets/$directory/js_files/results-$directory.sarif"
